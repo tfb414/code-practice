44192fb6f8cd46293c474b37914267251157a90e
Tim Brady <timbrady@Tims-MacBook-Pro-2.local>
2017-06-20 13:14:57 -0400

    worked on python projects

---
+++ DigitalCrafts/dayOfTheWeek.py
@@ -0,0 +1,7 @@
+day = int(raw_input('Day (0-6)? '))
+# week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"]
+if (day == 0 or day == 6):
+    print "sleep in"
+    
+else:
+    print "Go to work"
