56ed4f9928762bdd815afd6301896e1f0ace555e
Tim Brady <timbrady@Tims-MacBook-Pro-2.local>
2017-06-22 09:11:20 -0400

    completed additional python exercises

--- CodeWars/findUniqueNumber.py
+++ CodeWars/findUniqueNumber.py
@@ -1,9 +1,16 @@
 import re
 
 def find_uniq(arr):
+    def checkIfUnique(arr, n):
+        unique = True
+        for i in range(len(arr)):
+            if arr[i+1] == n
+            unique == false
+        return unique
+    
     arrList = list(arr)
-    myRegex = arrList[0]
-    derp = re.search(r'%d' % myRegex, arrList)
+    answer = filter()
+    
     print derp
 
 find_uniq([ 1, 1, 1, 2, 1, 1 ])
