b1a8a6102fd4736bddb8dcbc2d3060f114c9a073
tfb414 <tfb414@gmail.com>
2017-07-19 12:43:09 -0400

    creates js to create an array of 5 numbers

--- DigitalCrafts/Week5/generate_lotto_numbers/scripts/main.js
+++ DigitalCrafts/Week5/generate_lotto_numbers/scripts/main.js
@@ -1,4 +1,45 @@
 var testText = document.createElement("div");
 document.body.appendChild(testText);
 var div = document.querySelector("div");
-div.textContent = "this worked";
+div.textContent = createArrayOfNumbers(69)
+
+
+var numbersInLotto = createArrayOfNumbers(69);
+var firstFiveLottoArray = [0,0,0,0,0]
+
+function chooseARandomNumber(range){
+    var random = Math.floor(Math.random() * range);
+    return random;
+}
+
+function createArrayOfNumbers(totalNumbers){
+    var arrayOfNumbers = Array(totalNumbers).fill(totalNumbers);
+    totalNumbers += 1
+    var arrayOfNumbers = arrayOfNumbers.map(function(){
+        if (totalNumbers > 1){
+            totalNumbers -= 1;
+            return totalNumbers;
+        }
+    })
+    return arrayOfNumbers;
+}
+
+function chooseLottoNumber(numbersInLotto, lottoArray){
+    lottoArray.map(function(x){
+        var randomIndex = chooseARandomNumber(numbersInLotto.length);
+        
+        if(lottoArray.indexOf(numbersInLotto[randomIndex])){
+            console.log(numbersInLotto[randomIndex]);
+        return numbersInLotto[randomIndex];
+        
+        console.log(numbersInLotto[randomIndex]);
+        }
+    })    
+}
+
+
+
+chooseLottoNumber(numbersInLotto, firstFiveLottoArray);
+
+    
+
