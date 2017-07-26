1a62dadb23aa5e1cbd565c766f1a5086b66da32f
tfb414 <tfb414@gmail.com>
2017-07-26 09:05:41 -0400

    changes some of my functions to prototypes which may have been a mistake

--- DigitalCrafts/Week6/movieApp/scripts/main.js
+++ DigitalCrafts/Week6/movieApp/scripts/main.js
@@ -1,23 +1,90 @@
-console.log("derp");
-var URL = 'http://www.omdbapi.com/';
-var IMGURL = "http://img.omdbapi.com/";
+
+    var URL = 'http://www.omdbapi.com/';
+    var IMGURL = "http://img.omdbapi.com/";
 
 
-var $theForm = $('[data-movie="search"]');
-    // console.log($theForm);
+function MovieApp(){
+    this.URL = 'http://www.omdbapi.com/';
+    this.IMGURL = "http://img.omdbapi.com/";
+    this.$theForm = $('[data-movie="search"]');
+    this.test = 'it worked';
+    this.listOfMovies = [];
+    this.searchDataFromForm = {};
+}
+
+MovieApp.prototype.listOfMovies = function(){
+    this.listOfMovies.push(movie);
+    console.log(this.listOfMovies);
+// this.listOfMovies.push(this.grabAndFormatForm)
+//     console.log(this.listOfMovies);
+}
+
+MovieApp.prototype.grabAndFormatForm = function(){
+    this.$theForm.serializeArray().forEach(function(key){
+        this.searchDataFromForm[key.name] = key.value;
+        // console.log(this.$theForm.serializeArray());
+    }.bind(this));
     
-    $(document).ready(function() {
-        
-        $theForm.on('submit', function(event){
-            event.preventDefault();
-            var searchData = searchAPI(grabAndFormatForm($theForm));
+    
+}
+
+MovieApp.prototype.getRequestToServer = function(URL, Data){
+    return $.get(this.URL, data);
+}
+
+MovieApp.prototype.searchAPI() = function(){
+    var searchString = this.grabAndFormatForm['search'];
+    
+}
+
+function searchAPI(formObject){
+    var searchString = formObject['search'];
+    var data = {
+        "apikey": "1ca32dee",
+        "s": searchString
+
+    };
+    return data;
+ 
+
+}
+
+MovieApp.prototype.submit = function(event){
+    event.preventDefault();
+    this.grabAndFormatForm();
+    this.searchAPI();
+    // this.searchData = this.$theForm
+    // this.grabAndFormatForm;
+    // console.log(this.searchDataFromForm)
+
+}
+MovieApp.prototype.init = function(){
+        var form = this.$theForm;
+        form.on('submit', this.submit.bind(this));
+            // console.log(this.grabAndFormatForm);
+            // form.searchData = movies.$theForm
+            // var searchData = searchAPI(grabAndFormatForm($theForm));
             // console.log(searchData);
             
-            getRequestToServer(URL, searchData)
-                .then(listOfMovies);
-            $('[data-movie="search"]').trigger('reset');
-        });
-    });
+        //     // getRequestToServer(URL, searchData)
+        //     //     .then(listOfMovies);
+        //     // $('[data-movie="search"]').trigger('reset');
+    // });
+
+    
+    //manipulating the value of this within the anonymous function because each time you create a function it gets a new value of "this". 
+    //bind is going to pass down our bind
+
+}  
+
+
+
+
+
+var movies = new MovieApp() 
+// movies.init();
+document.addEventListener("DOMContentLoaded", function(){movies.init()});
+// $(document).ready(movies.init);
 
 
 function searchAPI(formObject){
@@ -32,25 +99,21 @@ function searchAPI(formObject){
 
 }
 
-function listOfMovies(movie){
-    var listOfMovies = []
-    listOfMovies.push(movie);
-    console.log(listOfMovies);
-    return listOfMovies
-}
 
-function grabAndFormatForm(data){
-    var dataObject = {};
-    data.serializeArray().forEach(function(key){
-        dataObject[key.name] = key.value;
-    })
-    console.log(dataObject)
-    return dataObject;
-}
 
-function getRequestToServer(URL, data){
-    return $.get(URL, data);
-}
+// function grabAndFormatForm(data){
+//     var dataObject = {};
+//     data.serializeArray().forEach(function(key){
+//         dataObject[key.name] = key.value;
+//     })
+//     console.log(dataObject)
+//     return dataObject;
+// }
+
+// function getRequestToServer(URL, data){
+//     return $.get(URL, data);
+// }
+
 
 
 
