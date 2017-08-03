d913591c7e0c34185afd04832ebee3cdb54db7d6
tfb414 <tfb414@gmail.com>
2017-08-03 10:02:16 -0400

    begins working on node exercises

---
+++ DigitalCrafts/Week7/markdown-to-pdf/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question('filename: ', (filename) => {
+    console.log("you said " + filename)
+    rl.close();
+    fs.readFile(filename, (err, buffer) => {
+        if(err){
+            console.log(err.message);
+            return;
+        }
+        let content = buffer.toString();
+        let upcased = content.toUpperCase();
+        console.log(upcased);
+        fs.writeFile(outputFile, upcased, (err) =>{
+            if(err){
+                console.log(err.message);
+                return;
+            }
+            console.log('it worked');
+        });
+    })
+    
+});
+
+// var yelp = (what) => {
+//     return "hey that " + what;
+// }
+
+// fs.watch('DERP.md', (event) => {
+//     // console.log('ouchies!');
+//     console.log(yelp('tickles derp'));
+//     console.log(event);
+// });
