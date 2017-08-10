323255128f3646a520c9e95749f216f34cde2fa4
tfb414 <tfb414@gmail.com>
2017-08-10 11:38:47 -0400

    one passing test

--- DigitalCrafts/Week8/lets-go-shopping/customer.js
+++ DigitalCrafts/Week8/lets-go-shopping/customer.js
@@ -1,7 +1,38 @@
 require('dotenv').config();
+const pg = require('pg-promise')();
+const dbConfig = {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    database: process.env.DB_NAME,
+};
+
+// const db = ;
+
+class Customer{
+    constructor(name, email, addr, password){
+        this.db = pg(dbConfig);
+        this.name = name;
+        this.email = email;
+        this.addr = addr;
+        this.password = password;
+    }
+    save(){
+        return this.db.query(`
+            insert into customers
+            (name, email, address, password)
+            values
+            ('${this.name}', '${this.email}', '${this.addr}', '${this.password}');
+        `);
+        //insert into customers is the column headings
+        //insert into values are the actual values
+    }
+}
+
+module.exports = Customer
 
 // var db = require('db')
 // db.connect({
 //   host: process.env.DB_HOST,
 //   username: process.env.DB_USER,
 // })
+
--- DigitalCrafts/Week8/lets-go-shopping/test/test-customers-model.js
+++ DigitalCrafts/Week8/lets-go-shopping/test/test-customers-model.js
@@ -1,7 +1,16 @@
 const expect = require('chai').expect;
+const Customer = require('../customer');
 
 describe('Customers', ()=> {
-    it('should be able to save to the database', ()=>{
+    it('should be able to save to the database', (done)=>{
         expect(1 + 1).to.equal(2);
-    })
-})
+
+        let myCustomer = new Customer('me', 'me@me.com', '123 me street', '123 me street', 'mememe');
+        myCustomer
+            .save()
+            .then((result)=>{
+                done();
+            });
+            
+    });
+});
