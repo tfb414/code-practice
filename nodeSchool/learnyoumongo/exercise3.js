1c7f387c07e88852653f8f7401b0793dbb0acfc4
Tim Brady <tfb414@gmail.com>
2017-10-30 18:19:19 -0400

    working on mongo

---
+++ nodeSchool/learnyoumongo/exercise3.js
@@ -0,0 +1,19 @@
+var mongo = require('mongodb').MongoClient
+let url = 'mongodb://localhost:27017/learnyoumongo';
+mongo.connect(url, function (err, db) {
+
+    var collection = db.collection('parrots');
+    collection.find().toArray(function (err, birds) {
+        // console.log(birds[0].age);
+
+        console.log(birds.map(function (bird) {
+            if (bird.age > parseInt(process.argv[2])) {
+                return bird;
+            }
+
+        }))
+        db.close();
+    }
+
+    )
+})
